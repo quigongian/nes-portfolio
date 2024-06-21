109f8d2f636003bf250bd0f7213b665239281809
quigongian <gipitz@protonmail.com>
2024-06-21 00:18:29 -0400

    Core View Framework created

---
+++ nes-project/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NavTest />
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import NavTest from './components/NavTest.vue';
+</script>
---
+++ nes-project/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="font-chicago">This is using the Inter font.</div>
+  </main>
+</template>
